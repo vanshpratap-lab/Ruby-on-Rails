b93fe73968e79a65c412bbecfb9c718afb40e886
student vanshpratap-lab <chauhanvanshpratapsingh@gmail.com>
2026-08-10 22:00:15 +0530

    implemented string methods

--- 1.rb
+++ 1.rb
@@ -1,3 +1,12 @@
-a = 3
-b = 4
-puts a + b
+# a = 3
+# b = 4
+# puts a + b
+
+name = "vansh"
+age = 19
+gpa = 8.5
+is_student = true 
+intro = " hello, my name is #{name} and my age is #{age} and i scored #{gpa} gpa "
+intro.upcase 
+puts intro.upcase
+puts intro.length
