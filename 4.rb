0ee1563e31295c9e824e41b44aede12dc0567547
student vanshpratap-lab <chauhanvanshpratapsingh@gmail.com>
2026-08-10 22:04:02 +0530

    implemented arrays and basic methods

---
+++ 4.rb
@@ -0,0 +1,13 @@
+# -----------ARRAY-----------
+list = ["donald trump", "putin", "modi ji", "meloni", ]
+# list.push("xi jimping")
+list << "kim jong un"
+puts list
+
+# -----------MEHTODS-----------
+def greet(user)
+  "welcome #{user}"
+end
+
+message = greet("vansh")
+puts message
