5b61461f3a2ee8a2f17f5f4b5f259871ed37cd0b
student vanshpratap-lab <chauhanvanshpratapsingh@gmail.com>
2026-08-10 22:03:09 +0530

    implemented methods and implicit return and control flow

---
+++ 3.rb
@@ -0,0 +1,26 @@
+# ------functional ruby-------
+# name = "Vansh"
+# puts "hello #{name}"
+
+# name = "Vansh"
+
+# def greet(name)
+#   puts "hello #{name}"
+# end
+
+# greet(name)
+
+# -------arithmatic part--------
+puts "please enter your marks"
+
+score = gets.chomp.to_i
+
+if score >= 90
+  puts "grade A"
+elsif score >= 80
+  puts "grade B"
+else 
+  puts "grade C"
+end
+
+
