7ef4055a5fd90c930aba11d1b58282f9dfe04091
student vanshpratap-lab <chauhanvanshpratapsingh@gmail.com>
2026-08-11 14:16:47 +0530

    implemented loops

---
+++ 5.rb
@@ -0,0 +1,9 @@
+# ---------LOOPS IN RUBY---------
+
+fruits = ["apple", "mango", "banana", "guava"]
+
+fruits.each do |fru|
+  puts "i like #{fru}"
+end
+
+fruits.each { |fru| puts " fruits : #{fru}"}
